0a0679cf708a60d0c7785503e61f0a1e7f371f4f
Matt <matt@mwmcguire.com>
2017-06-18 18:55:16 -0600

    Stable Dev Commit
    - Construction page up
    - All incomplete pages show construction page

--- blog/urls_blog.py
+++ blog/urls_blog.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-        #  url(r'^articles/', views.post_list, name='post_list'),
-        #  url(r'^projects/', views.about, name='projects'),
-        #  url(r'resume/', views.resume, name='resume'),
-        #  url(r'contact/', views.contact, name='contact'),
+        url(r'^articles/', views.post_list, name='post_list'),
+        url(r'^projects/', views.projects, name='projects'),
+        url(r'resume/', views.resume, name='resume'),
+        url(r'contact/', views.contact, name='contact'),
 ]
