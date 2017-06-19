b4a6fe0c49277b3bfab21075d6d554fb5854bdd8
Matt <matt@mwmcguire.com>
2017-06-18 23:34:00 -0600

    Change Some Content

--- blog/urls_blog.py
+++ blog/urls_blog.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
         url(r'^$', views.index, name='index'),
-        url(r'^home/$', views.index, name='index'),
+        url(r'home/$', views.index, name='index'),
         url(r'articles/$', views.post_list, name='post_list'),
         url(r'projects/$', views.projects, name='projects'),
         url(r'resume/$', views.resume, name='resume'),
