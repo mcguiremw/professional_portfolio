64216292e4c52733917f3ad85107add04c59248f
Me <matt@mwmcguire.com>
2017-08-20 14:57:25 +0000

    Update from prod

--- blog/views.py
+++ blog/views.py
@@ -31,4 +31,4 @@ def resume(request):
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
         'published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/construction.html', {'posts': posts})
